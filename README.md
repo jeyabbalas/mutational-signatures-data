4bdbddf44ea469f5f6b7d84046c999008919d213
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2022-02-15 09:56:11 -0500

    Tidying + documentation update.

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ The jupyter notebook `notebooks/Consuming ICGC Data.ipynb` shows how to consume 
 
 ## Processing ICGC simple somatic mutation data
 The jupyter notebook `notebooks/Processing ICGC Data.ipynb` shows how to process the simple somatic mutation (SSM) data that we downloaded from the ICGC Data Portal. After processing the SSM dataset, we generate a mutational spectra matrix, which tallies the counts for each context (as determined by the base on the 5' and the 3' ends of the mutated allele) of single base substitution. To obtain the context of the substitution, we require the GRCh37 reference genome. We obtain this genome via the [UCGC Genome Browser API](https://hgdownload.soe.ucsc.edu/downloads.html). The mutational spectra matrix for the BRCA datasets is stored in `data/mutational_spectra_wgs/`. This dataset can be readily used for mutational signature analysis.
+
+## Simulating MSK-IMPACT 410 gene panel data from whole genome MAF files
+The jupyter notebook `notebooks/Simulating MSK-IMPACT 410.ipynb` shows how to simulate a mutational spectra from MSK-IMPACT 410 gene panel from the MAF files generated from the ICGC SSM whole genome analysis dataset.
--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from pathlib import Path
+from typing import List
 
 import pandas as pd
 
 from lib import icgc, maf, msk_impact_410
+
+
+def download_icgc_ssm_as_mafs(dir_wgs: Path,
+                              projects: List[str],
+                              datatype: str,
+                              analysis_type: str,
+                              output_format: str,
+                              dir_maf_dirs: Path) -> None:
+    icgc.download_icgc_datasets(dir_wgs, projects, datatype, analysis_type, output_format)
+    maf.convert_ssms_to_mafs(dir_wgs, dir_maf_dirs)
+
+
+def download_grch37_reference_genome(fpath_compressed_grch37: Path,
+                                     fpath_grch37: Path) -> None:
+    maf.download_grch37(fpath_compressed_grch37)
+    maf.gunzip(fpath_compressed_grch37, fpath_grch37)
 
 
 if __name__ == '__main__':
@@ -16,22 +33,18 @@ if __name__ == '__main__':
     output_format = "TSV"
 
     dir_wgs = dir_data / "WGS"
-    icgc.download_icgc_datasets(dir_wgs, projects, datatype, analysis_type, output_format)
-
     dir_maf_dirs = dir_data / "WGS_MAFs"
     if not dir_maf_dirs.exists():
         dir_maf_dirs.mkdir()
-    maf.convert_ssms_to_mafs(dir_wgs, dir_maf_dirs)
+    download_icgc_ssm_as_mafs(dir_wgs, projects, datatype, analysis_type, output_format)
 
     fpath_compressed_grch37 = dir_data / "hg19.fa.gz"
-    maf.download_grch37(fpath_compressed_grch37)
     fpath_grch37 = dir_data / "hg19.fa"
-    maf.gunzip(fpath_compressed_grch37, fpath_grch37)
+    download_grch37_reference_genome(fpath_compressed_grch37, fpath_grch37)
 
     dir_spectra = dir_data / "mutational_spectra_wgs"
     if not dir_spectra.exists():
         dir_spectra.mkdir()
-
     maf.convert_maf_dirs_to_sbs_mutational_spectra(dir_maf_dirs, fpath_grch37, dir_spectra)
 
     dir_panel_maf_dirs = dir_data / "MSK_IMPACT_410_MAFs"
